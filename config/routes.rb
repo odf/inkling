426ef6aa1130671df963946932fd4133df7a5b3d
biv <nicholas.faiz@gmail.com>
2011-03-08 11:50:33 +1100

    inkling takes user_root path, hopefully the owning rails app can override this path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do |map|
+    
   namespace :inkling do
     resources :paths, :themes
     match 'update_tree' => 'paths#update_tree', :as => :update_tree    
@@ -9,11 +10,7 @@ Rails.application.routes.draw do |map|
       match 'content_types' => 'content_types#index', :as => :content_types
     end
 
-    match 'home', :to => 'inkling/home#dashboard', :as => "inkling_user_root"  
+    match 'home', :to => 'home#dashboard', :as => "user_root"  
     devise_for "users", :controllers => { :sessions => "inkling/users/sessions", :passwords => "inkling/users/passwords", :confirmations => "inkling/users/confirmations"}, :class_name => "Inkling::User"
   end
-      
-  # namespace :inkling do
-  #   devise_for "users", :controllers => { :sessions => "inkling/users/sessions", :passwords => "inkling/users/passwords", :confirmations => "inkling/users/confirmations"}, :class_name => "Inkling::User"
-  # end 
 end
