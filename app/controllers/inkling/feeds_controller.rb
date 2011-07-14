edb66df583727976f3b8d5a7b10eb83b5cc52862
Nicholas Faiz <nicholas.faiz@gmail.com>
2011-07-14 14:34:54 +1000

    working on feeds again

--- app/controllers/inkling/feeds_controller.rb
+++ app/controllers/inkling/feeds_controller.rb
@@ -1,13 +1,9 @@
 class Inkling::FeedsController < ApplicationController
   
-  #this assumes a format of xml. For other formats simply inherit from the controller and override show
+  respond_to :rss, :atom, :xml
+
   def show
     @feed = Inkling::Feed.find(params[:id])
-    
-    respond_to do |format|
-      format.xml do
-        render :xml => @feed.generate, :layout => false
-      end
-    end
+    respond_with @feed.generate, :layout => false
   end
 end
